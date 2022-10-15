89cf7619ebcab50f0f848db0b8a178b1bf88005b
Alexandra Maroco <alexandra.maroco@tecnico.ulisboa.pt>
2022-10-15 10:55:14 +0100

    applying css

--- korber-challenge/src/App.vue
+++ korber-challenge/src/App.vue
@@ -1,9 +1,28 @@
 <template>
-  <nav>
+  <!--<nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <!--<AppHeader /> -->
+  <AppHeader /> -->
+
+  <header class="top-bar spread">
+    <nav class="top-bar-nav">
+      <router-link to="/" class="top-bar-link">
+        <i class="icofont-spoon-and-fork"></i>
+        <span>Home</span>
+      </router-link>
+      <router-link to="/products" class="top-bar-link">
+        <span>Products</span>
+      </router-link>
+      <router-link to="/past-orders" class="top-bar-link">
+        <span>Past Orders</span>
+      </router-link>
+    </nav>
+    <div @click="toggleSidebar" class="top-bar-cart-link">
+      <i class="icofont-cart-alt icofont-1x"></i>
+      <span>Cart ({{ totalQuantity }})</span>
+    </div>
+  </header>
 
   <router-view/>
 </template>
@@ -27,6 +46,7 @@ export default {
 
 </script>
 
+<!--
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -48,4 +68,4 @@ nav {
     }
   }
 }
-</style>
+</style> -->
